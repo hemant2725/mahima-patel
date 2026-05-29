ea886c3d1467c0ede9f7687aaef90dc1fe467145
hemant2725 <hs878752@gmail.com>
2026-05-30 00:24:34 +0530

    prepare deploy build

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
-import { dirname, resolve } from "path"
-import { fileURLToPath } from "url"
+import path from "node:path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": path.resolve(process.cwd(), "src"),
     },
   },
 });
